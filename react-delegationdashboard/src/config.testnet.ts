7ad0719987a122f14d7dd8bef619afd865cf6645
Smart Chain Connection <contact@smartchainconnection.com>
2021-07-29 05:42:17 -0700

    New SC-5-42

--- react-delegationdashboard/src/config.testnet.ts
+++ react-delegationdashboard/src/config.testnet.ts
@@ -39,7 +39,7 @@ export const network: NetworkType = {
   apiAddress: 'https://testnet-api.elrond.com',
   gatewayAddress: 'https://testnet-gateway.elrond.com',
   explorerAddress: 'http://testnet-explorer.elrond.com/',
-  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqp0llllswfeycs',
+  delegationContract: '000000000000000000010000000000000000000000000000000000000effffff',
 };
 
 const networkSchema = object({
